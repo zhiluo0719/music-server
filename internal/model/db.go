629530b85559282f912cdff87c5a78cf7207adb9
zhiluo0719 <144047862+zhiluo0719@users.noreply.github.com>
2026-07-05 00:04:43 +0800

    Add internal/model/db.go

---
+++ internal/model/db.go
@@ -0,0 +1,34 @@
+﻿package model
+
+import (
+	"log"
+	"os"
+	"path/filepath"
+
+	"github.com/glebarez/sqlite"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+var DB *gorm.DB
+
+func InitDB(dsn string) {
+	dir := filepath.Dir(dsn)
+	if err := os.MkdirAll(dir, 0755); err != nil {
+		log.Fatalf("创建数据库目录失败: %v", err)
+	}
+
+	var err error
+	DB, err = gorm.Open(sqlite.Open(dsn), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Info),
+	})
+	if err != nil {
+		log.Fatalf("连接数据库失败: %v", err)
+	}
+
+	if err := DB.AutoMigrate(&Song{}, &Playlist{}, &PlaylistSong{}); err != nil {
+		log.Fatalf("数据库迁移失败: %v", err)
+	}
+
+	log.Println("数据库初始化完成")
+}
